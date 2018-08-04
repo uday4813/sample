b882bf1524cd55236005b8a052d4033363baca8a
Uday <uday@gmail.com>
2018-08-04 11:44:38 +0530

    test1

---
+++ src/main/java/jar/DemoLayout.java
@@ -0,0 +1,25 @@
+package jar;
+
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class DemoLayout extends VerticalLayout {
+
+    public DemoLayout() {
+        setSpacing(true);
+        setMargin(true);
+        setSizeFull();
+
+        final Button hello = new Button("Say hello");
+        hello.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+                Notification.show("Hello World!");
+            }
+        });
+        addComponent(hello);
+        setComponentAlignment(hello, Alignment.MIDDLE_CENTER);
+    }
+}
---
+++ src/test/java/jar/DemoApp.java
@@ -0,0 +1,15 @@
+package jar;
+
+import com.bsb.common.vaadin.embed.support.EmbedVaadin;
+
+public class DemoApp {
+
+
+    public static void main(String[] args) {
+
+        EmbedVaadin.forComponent(new DemoLayout())
+                .withDevelopmentHeader(true)
+                .openBrowser(true)
+                .start();
+    }
+}
